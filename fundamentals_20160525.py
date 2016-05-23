a2e2bc6f3502b43bd7920969d35988d800d25b2a
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-05-23 13:17:57 +0900

    Add matplotlib and python information

--- fundamentals_20160525.py
+++ fundamentals_20160525.py
@@ -1,3 +1,4 @@
+######################## numpy ############################
 # numpyの標準的な使い方
 import numpy as np
 
@@ -15,10 +16,10 @@ str_array = my_array.astype(str)
 
 # これでarrayの差分が表示されます
 for i in my_array:
-    print i*4
+    print(i*4)
 
 for i in str_array:
-    print i*4
+    print(i*4)
 
 
 # これは多少listですね。numpyは複数次元arrayにもっと便利です。
@@ -53,4 +54,45 @@ np.savetxt('lookatarray.csv', dim2_array, delimiter = ',')
 # csvを開く
 np.genfromtxt('lookatarray.csv', delimiter = ',')
 
-# 画像表示を追加する
+
+######################## matplotlib ############################
+# matplotlibとnumpyの全ての関数をglobal namespaceにimportする
+# 危険なので、テスト以外は使わないでください。
+from pylab import *
+# -20から20のiteratorを作成
+x = range(-20, 20)
+# iterate over x! これは死ぬほど便利です。
+y = [i*i for i in x]
+# 曲線を描画する
+plot(x, y)
+# 表示する
+show()
+
+################# matplotlibにおける画像処理の簡単な事例 ###########
+
+# 曲線の描画moduleをインポート。表示としてpltと名づけます。
+import matplotlib.pyplot as plt
+# 画像処理moduleをインポート
+import matplotlib.image as mpimg
+# 画像を読み込む
+img = mpimg.imread('stinkbug.png')
+# 画像を描画
+plt.imshow(img)
+# 新しいWindowを作る
+plt.figure()
+# sliceのパワー：RGBからRだけを残す。lum_imgは二次元arrayになりました。
+lum_img = img[:,:,0]
+# 現在のWindow（＝Figure）画像を描画
+plt.imshow(lum_img, cmap="hot")
+# colorbarを追加する
+plt.colorbar()
+# 新しいWindowを作る
+plt.figure()
+# 画像のhistogramを作る
+plt.hist(lum_img.ravel(), bins=256, range=(0.0, 1.0))
+plt.figure()
+# histogramのデータを考慮し、コントラストを増加しよう。
+plt.imshow(lum_img, clim=(0.0, 0.7))
+plt.colorbar()
+# 結局、全てのWindowを表示する
+plt.show()
